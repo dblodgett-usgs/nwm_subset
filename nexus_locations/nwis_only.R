3519eca0ec96ab1d0704215ecccf82b3703506e4
David Blodgett <dblodgett@usgs.gov>
2018-05-22 10:29:51 -0500

    updated nwis_comids

--- nexus_locations/nwis_only.R
+++ nexus_locations/nwis_only.R
@@ -1,3 +1,10 @@
-sites <- sf::st_read("linked_sites.gpkg")
+sites <- sf::st_read("nexus_locations/linked_sites.gpkg")
 comids <- sites$COMID
+
+v1_comids <- readRDS("nexus_locations/nwm_v1_comids.rds")
+v2_comids <- ncdf4::nc_open("NWM_v1.2_nc_tools_v1/spatialMetadataFiles/nwm-v1.2-channel_spatial_index.nc")$dim$feature_id$vals
+
+comids <- comids[comids %in% v1_comids]
+comids <- comids[comids %in% v2_comids]
+
 saveRDS(comids, "nwis_comids.rds")
