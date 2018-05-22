c266b004ff5c0d8b02f1f0a3db31911c064209d7
David L. Blodgett <david.blodgett@nwcalusers-imac.nwc.nws.noaa.gov>
2018-05-22 13:28:43 -0500

    use ostore

--- nwm_cull/dl_ostore.R
+++ nwm_cull/dl_ostore.R
@@ -1,6 +1,18 @@
-date <- "20180101"
-config <- "long_range_mem1"
-type <- "channel_rt"
+#!/usr/bin/env Rscript
+
+# date <- "20180505"
+# config <- "long_range_mem1"
+# type <- "channel_rt"
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)<3) {
+  stop("Need date, config, and type.\n", call.=FALSE)
+} else if (length(args)==3) {
+  date <- args[[1]]
+  config <- args[[2]]
+  type <- args[[3]]
+}
 
 library(xml2)
 
@@ -19,16 +31,19 @@ get_ostore_urls <- function(date, config, type) {
   return(paste0(paste0(ostore_root, "/"), objects[grepl(date, objects) & grepl(config, objects) & grepl(type, objects)]))
 }
 
-dl_list <- get_ostore_urls(date, config, type)
-
 dl_ostore_list <- function(dl_list) {
-  out_files <- stringr::str_remove(dl_list, ostore_root)
+  out_files <- paste0("nwm", stringr::str_remove(dl_list, ostore_root))
   
-  direc <- paste0(stringr::str_split(out_files[1], "/")[[1]][1:2], collapse = "/")
+  direc <- paste0(stringr::str_split(out_files[1], "/")[[1]][1:3], collapse = "/")
   
   dir.create(direc, recursive = T, showWarnings = F)
   
   for(i in 1:length(dl_list)) {
-    download.file(dl_list[i], out_files[i])
+    download.file(dl_list[i], out_files[i], quiet = T)
   }
 }
+
+dl_ostore_list(get_ostore_urls(date, config, type))
+
+
+
