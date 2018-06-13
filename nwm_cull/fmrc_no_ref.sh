ea3b8954376d48efdc6dee891f86c910ec77c6a9
David Blodgett <dblodgett@usgs.gov>
2018-06-13 10:53:54 -0500

    catch up commit

--- nwm_cull/fmrc_no_ref.sh
+++ nwm_cull/fmrc_no_ref.sh
@@ -4,22 +4,20 @@ function ncattget { ncks --trd -M -m ${3} | grep -E -i "^${2} attribute [0-9]+: 
 
 in_dir=$1
 out_file=$2
-temp_dir=$3
 
 if [ -f $out_file ]; then
 	exit 0
 fi
 touch $out_file
 
-ff=${temp_dir}/$out_file
 in_files=${in_dir}/*.nc
 in_files=( $in_files )
 sf=$(ncattget scale_factor streamflow ${in_files[0]}) # need to do this because the calls below blow it away.
 for f in ${in_dir}/*.nc; do
   ncks -h --exclude -v nudge,velocity,q_lateral,qBtmVertRunoff,qBucket,qSfcLatRunoff -O $f $f
   ncap2 -h -O -s 'streamflow[$time,$feature_id]=streamflow' $f $f
-  ncatted -h -O -a "scale_factor,streamflow,o,f,$sf" $f $f
 done
-ncrcat -h ${in_dir}/*.nc $ff # > /dev/null 2>&1
-nccopy -k nc3 $ff $out_file
-rm $ff
+ncrcat -h -O -6 ${in_dir}/*.nc $out_file # > /dev/null 2>&1
+ncks -h -O --fix_rec_dmn time $out_file $out_file
+ncap2 -h -O -s 'streamflow[$reference_time,$time,$feature_id]=streamflow; time[$reference_time,$time]=time' $out_file $out_file
+ncatted -h -O -a "scale_factor,streamflow,o,f,$sf" $out_file $out_file
