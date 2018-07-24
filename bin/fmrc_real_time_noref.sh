f93ba6f945dd0b10c4be619cebe7611cc8c2d2e4
David Blodgett <dblodgett@usgs.gov>
2018-07-24 16:13:39 -0500

    will improve ncattget later

--- bin/fmrc_real_time_noref.sh
+++ bin/fmrc_real_time_noref.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-source ncattget.sh
+ncks_version=$(ncks --version 2>&1 | grep ncks | grep -o [0-9].[0-9].[0-9])
+ncks_version=${ncks_version: -3}
+check_version="${ncks_version} > 6.8"
+
+if [ $(echo $check_version | bc) -eq 1 ]; then
+  function ncattget { ncks --trd -M -m ${3} | grep -E -i "^${2} attribute [0-9]+: ${1}" | cut -f 11- -d ' ' | sort ; }
+else
+  function ncattget { ncks -M -m ${3} | grep -E -i "^${2} attribute [0-9]+: ${1}" | cut -f 11- -d ' ' | sort ; }
+fi
 
 in_dir=$1 # this folder should contain one model run of NWM output for one configuration.
 out_file=$2 # pick your poison
