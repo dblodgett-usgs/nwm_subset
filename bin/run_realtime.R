e32ef6ea68e793fd0ee90f85839e1c7f755ed8f7
David Blodgett <dblodgett@usgs.gov>
2018-07-24 13:48:11 -0500

    reorganize run_realtime and add retry functionality

--- bin/run_realtime.R
+++ bin/run_realtime.R
@@ -8,20 +8,6 @@ if (length(args)<1) {
   configuration <- args[[1]]
 }
 
-proc_dir <- "./data/proc"
-data_dir <- "./data/"
-lat_lon_file <- "../NWM_v1.2_nc_tools_v1/spatialMetadataFiles/nwm-v1.2-channel_spatial_index.nc"
-
-nomads_url <- "http://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod"
-wget_base <- "wget -r -np -nH --cut-dirs=8 -q -A"
-file_type <- "channel_rt"
-
-day_folder <- paste0("nwm.", format((Sys.time()-60), "%Y%m%d", tz = "UTC"))
-
-####
-# configuration <- "mr"
-####
-
 t1 <- c("t00z", "t06z", "t12z", "t18z")
 t2 <- c("t00z", "t01z", "t02z", "t03z", "t04z", 
         "t05z", "t06z", "t07z", "t08z", "t09z", 
@@ -29,36 +15,32 @@ t2 <- c("t00z", "t01z", "t02z", "t03z", "t04z",
         "t15z", "t16z", "t17z", "t18z", "t19z", 
         "t20z", "t21z", "t22z", "t23z")
 
-times <- list(mr = t1, aa = t2, sr = t2,
+times <- list(aa = t2, sr = t2, mr = t1,
               lr1 = t1, lr2 = t1, lr3 = t1, lr4 = t1)
 
-config_lookup <- list(mr = list(dir = "medium_range", 
-                                fi = "medium_range"),
-                      aa = list(dir = "analysis_assim", 
-                                fi = "analysis_assim"),
-                      sr = list(dir = "short_range", 
-                                fi = "short_range"),
-                      lr1 = list(dir = "long_range_mem1", 
-                                 fi = "long_range_mem1"),
-                      lr2 = list(dir = "long_range_mem2", 
-                                 fi = "long_range_mem2"),
-                      lr3 = list(dir = "long_range_mem3", 
-                                 fi = "long_range_mem3"),
-                      lr3 = list(dir = "long_range_mem4", 
-                                 fi = "long_range_mem4"))
+retry_dir <- "./data/retry"
 
-# Gotta figure out date/time handling.
-hour_string <- paste0("t", format((Sys.time() - 3600), "%H", tz = "UTC"), "z")
-
-if(hour_string %in% times[[configuration]]) {
+run_func <- function(configuration, day_folder, hour_string, retry_dir) {
   
-  # ####
-  # day_folder <- "nwm.20180723"
-  # ####
-  unlink(proc_dir, recursive = TRUE)
+  config_lookup <- 
+    list(aa = list(dir = "analysis_assim", fi = "analysis_assim", exp_fis = 3),
+         sr = list(dir = "short_range", fi = "short_range", exp_fis = 18),
+         mr = list(dir = "medium_range", fi = "medium_range", exp_fis = 80),
+         lr1 = list(dir = "long_range_mem1", fi = "long_range_mem1", exp_fis = 120),
+         lr2 = list(dir = "long_range_mem2", fi = "long_range_mem2", exp_fis = 120),
+         lr3 = list(dir = "long_range_mem3", fi = "long_range_mem3", exp_fis = 120),
+         lr3 = list(dir = "long_range_mem4", fi = "long_range_mem4", exp_fis = 120))
+  
+  nomads_url <- "http://nomads.ncep.noaa.gov/pub/data/nccf/com/nwm/prod"
+  wget_base <- "wget -r -np -nH --cut-dirs=8 -q -A"
+  file_type <- "channel_rt"
+  data_dir <- "./data/"
+  proc_dir <- paste0("./data/proc", as.numeric(Sys.time()))
+  lat_lon_file <- "../NWM_v1.2_nc_tools_v1/spatialMetadataFiles/nwm-v1.2-channel_spatial_index.nc"
+  
   dir.create(proc_dir, showWarnings = FALSE, recursive = TRUE)
-  
-  system(sprintf('%s "*%s.%s.%s*" -P %s %s/%s/%s/', 
+  # should do a try here and redirect any errors to log.
+  system(sprintf('%s "*%s.%s.%s*" -P %s %s/%s/%s/',
                  wget_base,
                  hour_string,
                  config_lookup[[configuration]]$fi,
@@ -68,21 +50,49 @@ if(hour_string %in% times[[configuration]]) {
                  day_folder,
                  config_lookup[[configuration]]$dir))
   
-  out_dir <-  paste0(data_dir, config_lookup[[configuration]]$dir)
-  out_dir_noref <-paste0(out_dir, "/noref")
-  dir.create(out_dir_noref, showWarnings = FALSE, recursive = TRUE)
-  out_dir_ref <- paste0(out_dir, "/ref")
-  dir.create(out_dir_ref, showWarnings = FALSE, recursive = TRUE)
-  
-  out_file_noref <- sprintf("%s/%s.nc", out_dir_noref, hour_string)
-  out_file_ref <- sprintf("%s/%s.nc", out_dir_ref, hour_string)
-  
-  system(sprintf("../bin/fmrc_real_time_both.sh %s %s %s %s", 
-                 proc_dir, 
-                 out_file_noref, 
-                 out_file_ref, 
-                 lat_lon_file))
+  if(length(list.files(proc_dir) == config_lookup[[configuration]]$exp_fis)) {
+    
+    out_dir <-  paste0(data_dir, config_lookup[[configuration]]$dir)
+    out_dir_noref <-paste0(out_dir, "/noref")
+    dir.create(out_dir_noref, showWarnings = FALSE, recursive = TRUE)
+    out_dir_ref <- paste0(out_dir, "/ref")
+    dir.create(out_dir_ref, showWarnings = FALSE, recursive = TRUE)
+    
+    out_file_noref <- sprintf("%s/%s.nc", out_dir_noref, "latest")
+    out_file_ref <- sprintf("%s/%s.nc", out_dir_ref, hour_string)
+    
+    system(sprintf("../bin/fmrc_real_time_both.sh %s %s %s %s",
+                   proc_dir,
+                   out_file_noref,
+                   out_file_ref,
+                   lat_lon_file))
+    
+  } else {
+    
+    dir.create(retry_dir, showWarnings = FALSE, recursive = TRUE)
+    system(sprintf("touch %s/%s__%s__%s", retry_dir, configuration, day_folder, hour_string))
+    
+  }
   
   unlink(proc_dir, recursive = TRUE)
   
 }
+
+day_folder <- paste0("nwm.", format((Sys.time() - 3600), "%Y%m%d", tz = "UTC"))
+hour_string <- paste0("t", format((Sys.time() - 3600), "%H", tz = "UTC"), "z")
+
+if(hour_string %in% times[[configuration]]) {
+  run_func(configuration, day_folder, hour_string, retry_dir)
+}
+
+for(retry_run in list.files(retry_dir)) {
+
+  unlink(file.path(retry_dir, retry_run))
+    
+  retry_run <- strsplit(retry_run, "__")[[1]]
+  
+  run_func(retry_run[1], retry_run[2], retry_run[3], retry_dir)
+
+}
+
+
